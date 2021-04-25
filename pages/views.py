5459d4758a17f65c8ed88c9f617c19c5cab5a57e
kmuni08 <Karishma.Muni24@gmail.com>
2021-04-25 16:00:08 -0400

    Error text changed

--- pages/views.py
+++ pages/views.py
@@ -48,7 +48,7 @@ def registerUser(request):
         serializer = UserSerializerWithToken(user, many=False)
         return Response(serializer.data)
     except:
-        message = {'detail': 'User with this email already exists'}
+        message = {'detail': 'User with this email already exists. Pick a new one. '}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
